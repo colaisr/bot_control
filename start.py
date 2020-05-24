e88ef6e88486d860afa41cd21e2bc621888a4a94
colakamornik <cola.isr@gmail.com>
2020-05-24 11:55:35 +0300

    Bots are added to db relationaly to user

--- start.py
+++ start.py
@@ -1,11 +1,13 @@
 import configparser
 import datetime
 
-from flask import Flask, render_template, render_template_string
+from flask import Flask, render_template, render_template_string, request, url_for
 from flask_babelex import Babel
+from flask_login import current_user
 from flask_sqlalchemy import SQLAlchemy
-
 # Class-based application configuration
+from werkzeug.utils import redirect
+
 from flask_user import UserMixin, UserManager, login_required, roles_required
 
 
@@ -31,9 +33,9 @@ class ConfigClass(object):
 
     # Flask-User settings
     USER_APP_NAME = "Bot Control"  # Shown in and email templates and page footers
-    USER_APP_VERSION="1.01"
-    USER_COPYRIGHT_YEAR="2020"
-    USER_CORPORATION_NAME="BotGeeks"
+    USER_APP_VERSION = "1.01"
+    USER_COPYRIGHT_YEAR = "2020"
+    USER_CORPORATION_NAME = "BotGeeks"
     USER_ENABLE_EMAIL = True  # Enable email authentication
     USER_ENABLE_USERNAME = False  # Disable username authentication
     USER_EMAIL_SENDER_NAME = USER_APP_NAME
@@ -69,6 +71,8 @@ class User(db.Model, UserMixin):
 
     # Define the relationship to Role via UserRoles
     roles = db.relationship('Role', secondary='user_roles')
+    # Define the relationship to Bots via UserBots
+    bots = db.relationship('Bot', secondary='user_bots')
 
 
 # Define the Role data-model
@@ -84,6 +88,30 @@ class UserRoles(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+
+# Define the UserBots association table
+class UserBots(db.Model):
+    __tablename__ = 'user_bots'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    bot_id = db.Column(db.Integer(), db.ForeignKey('bots.id', ondelete='CASCADE'))
+
+
+class Bot(db.Model, UserMixin):
+    __tablename__ = 'bots'
+    id = db.Column(db.Integer, primary_key=True)
+    running = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+
+    # User authentication information. The collation='NOCASE' is required
+    # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
+    name = db.Column(db.String(255, collation='NOCASE'), nullable=False, server_default='')
+    email_confirmed_at = db.Column(db.DateTime())
+    password = db.Column(db.String(255), nullable=False, server_default='')
+
+    # User information
+    first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
 
 
 # Setup Flask-User and specify the User data-model
@@ -118,25 +146,38 @@ if not User.query.filter(User.email == 'admin@example.com').first():
 # The Home page is accessible to anyone
 @app.route('/')
 def home_page():
-    return render_template('home.html')
-    # return render_template_string("""
-    #         {% extends "flask_user_layoutN.html" %}
-    #         {% block content %}
-    #
-    #             <h2>{%trans%}Home page{%endtrans%}</h2>
-    #             <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
-    #             <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
-    #             <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
-    #             <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
-    #             <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
-    #             <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
-    #         {% endblock %}
-    #         """)
+    user = current_user
+    if user.is_authenticated:
+        return redirect(url_for('bots_page'))
+    else:
+        return render_template('home.html')
 
 
-# @app.route('/')
-# def home():
-#     retdurn render_template('home.html')
+# The Bots page showing the bots per user
+@app.route('/bots')
+def bots_page():
+    user_id = current_user.id
+    user_in_db = User.query.filter(User.id == user_id).first()
+    bots = user_in_db.bots
+    return render_template('bots.html', bots=bots)
+
+
+@app.route('/createbot', methods=['POST'])
+def create_bot():
+    user_id = current_user.id
+    bot_name = request.form['botname']
+
+    bot = Bot(
+        name=bot_name,
+
+    )
+    user_in_db = User.query.filter(User.id == user_id).first()
+    user_in_db.bots.append(bot)
+    db.session.add(bot)
+    db.session.commit()
+
+    return redirect(url_for('bots_page'))
+
 
 # The Members page is only accessible to authenticated users
 @app.route('/members')
