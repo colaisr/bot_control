1961f514b8f8c4d29e83cf5524d95b5c7e18a194
lili963 <liliana.isr@gmail.com>
2020-06-04 19:33:43 +0300

    lili: registration

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,29 @@
-import configparser
-import datetime
-
-from flask import Flask, render_template_string, request, url_for
-from flask_babelex import Babel
-# from flask_login import current_user
-# from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-# Class-based application configuration
-from werkzeug import utils
-from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired
-from wtforms.widgets import HiddenInput
-
-from Bots import bot_tele
-# from flask_user import UserMixin, UserManager, login_required, roles_required
-# from flask_user import UserManager, login_required, roles_required
+# import configparser
+# import datetime
+#
+# from flask import Flask, render_template_string, request, url_for
+# from flask_babelex import Babel
+# # from flask_login import current_user
+# # from flask_sqlalchemy import SQLAlchemy
+# from flask_wtf import FlaskForm
+# # Class-based application configuration
+# from werkzeug import utils
+# from wtforms import StringField, SubmitField, IntegerField
+# from wtforms.validators import DataRequired
+# from wtforms.widgets import HiddenInput
+#
+# from Bots import bot_tele
+# # from flask_user import UserMixin, UserManager, login_required, roles_required
+# # from flask_user import UserManager, login_required, roles_required
 
 # LILI imports
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
 from app import db
-
 
 # The Home page is accessible to anyone
 @app.route('/')
@@ -67,9 +66,23 @@ def logout():
     logout_user()
     return redirect(url_for('home_page'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 
 # Initialize Flask-BabelEx
-babel = Babel(app)
+# babel = Babel(app)
 
 # Initialize Flask-SQLAlchemy
 # db = SQLAlchemy(app)
@@ -82,40 +95,40 @@ ALL_RUNNING_BOTS = {}
 # Create all database tables
 # db.create_all()
 
-# Create 'member@example.com' user with no roles
-if not User.query.filter(User.email == 'member@example.com').first():
-    user = User(
-        email='member@example.com',
-        email_confirmed_at=datetime.datetime.utcnow()
-        # password_hash=user_manager.hash_password('Password1'),
-    )
-    user.set_password('Password1');
-    db.session.add(user)
-    db.session.commit()
-
-# Create 'admin@example.com' user with 'Admin' and 'Agent' roles
-if not User.query.filter(User.email == 'admin@example.com').first():
-    user = User(
-        email='admin@example.com',
-        email_confirmed_at=datetime.datetime.utcnow()
-        # password_hash=user_manager.hash_password('Password1'),
-    )
-    user.set_password('Password1');
-    # user.roles.append(Role(name='Admin'))
-    # user.roles.append(Role(name='Agent'))
-    db.session.add(user)
-    db.session.commit()
-
-
-class BotCreateForm(FlaskForm):
-    name = StringField('Name', [DataRequired()])
-    api_key = StringField('api_key')
-    calendar_id = StringField('calendar_id')
-    submit = SubmitField('Submit')
-
-
-class BotUpdateForm(BotCreateForm):
-    id = IntegerField(widget=HiddenInput())
+# # Create 'member@example.com' user with no roles
+# if not User.query.filter(User.email == 'member@example.com').first():
+#     user = User(
+#         email='member@example.com',
+#         email_confirmed_at=datetime.datetime.utcnow()
+#         # password_hash=user_manager.hash_password('Password1'),
+#     )
+#     user.set_password('Password1');
+#     db.session.add(user)
+#     db.session.commit()
+#
+# # Create 'admin@example.com' user with 'Admin' and 'Agent' roles
+# if not User.query.filter(User.email == 'admin@example.com').first():
+#     user = User(
+#         email='admin@example.com',
+#         email_confirmed_at=datetime.datetime.utcnow()
+#         # password_hash=user_manager.hash_password('Password1'),
+#     )
+#     user.set_password('Password1');
+#     # user.roles.append(Role(name='Admin'))
+#     # user.roles.append(Role(name='Agent'))
+#     db.session.add(user)
+#     db.session.commit()
+#
+#
+# class BotCreateForm(FlaskForm):
+#     name = StringField('Name', [DataRequired()])
+#     api_key = StringField('api_key')
+#     calendar_id = StringField('calendar_id')
+#     submit = SubmitField('Submit')
+#
+#
+# class BotUpdateForm(BotCreateForm):
+#     id = IntegerField(widget=HiddenInput())
 
 
 # @app.route('/createbotform', methods=('GET', 'POST'))
@@ -204,47 +217,47 @@ class BotUpdateForm(BotCreateForm):
 #     return utils.redirect(url_for('bots_page'))
 
 
-# The Members page is only accessible to authenticated users
-@app.route('/members')
-@login_required  # Use of @login_required decorator
-def member_page():
-    return render_template_string("""
-            {% extends "flask_user_layoutN.html" %}
-            {% block content %}
-                <h2>{%trans%}Members page{%endtrans%}</h2>
-                <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
-                <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
-                <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
-                <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
-                <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
-                <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
-            {% endblock %}
-            """)
-
-
-# hm
-# The Admin page requires an 'Admin' role.
-@app.route('/admin')
-# @roles_required('Admin')  # Use of @roles_required decorator
-def admin_page():
-    return render_template_string("""
-            {% extends "flask_user_layout.html" %}
-            {% block content %}
-                <h2>{%trans%}Admin Page{%endtrans%}</h2>
-                <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
-                <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
-                <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
-                <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
-                <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
-                <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
-            {% endblock %}
-            """)
-
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     return render_template('errors/404.html'), 404
-
-
-# @app.errorhandler(500)
-# def internal_error(error):
-#     return render_template('errors/page_not_found.html'), 404
+# # The Members page is only accessible to authenticated users
+# @app.route('/members')
+# @login_required  # Use of @login_required decorator
+# def member_page():
+#     return render_template_string("""
+#             {% extends "flask_user_layoutN.html" %}
+#             {% block content %}
+#                 <h2>{%trans%}Members page{%endtrans%}</h2>
+#                 <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
+#                 <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
+#                 <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
+#                 <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
+#                 <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
+#                 <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
+#             {% endblock %}
+#             """)
+#
+#
+# # hm
+# # The Admin page requires an 'Admin' role.
+# @app.route('/admin')
+# # @roles_required('Admin')  # Use of @roles_required decorator
+# def admin_page():
+#     return render_template_string("""
+#             {% extends "flask_user_layout.html" %}
+#             {% block content %}
+#                 <h2>{%trans%}Admin Page{%endtrans%}</h2>
+#                 <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
+#                 <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
+#                 <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
+#                 <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
+#                 <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
+#                 <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
+#             {% endblock %}
+#             """)
+#
+# # @app.errorhandler(404)
+# # def page_not_found(error):
+# #     return render_template('errors/404.html'), 404
+#
+#
+# # @app.errorhandler(500)
+# # def internal_error(error):
+# #     return render_template('errors/page_not_found.html'), 404
